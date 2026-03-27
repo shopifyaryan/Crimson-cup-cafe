bb20d107f8be4901e1cfb3b681ca48ef8b360c74
shopifyaryan <shopifyaryan82@gmail.com>
2026-03-27 19:06:24 +0530

    Refactor code structure for improved readability and maintainability

--- swiper.js
+++ swiper.js
@@ -19,4 +19,32 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+var swiper = new Swiper(".eventSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        600: {
+            slidesPerView: 2
+        },
+        992: {
+            slidesPerView: 3
+        }
+    }
+});
+
 
