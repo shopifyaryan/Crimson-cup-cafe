6d1eb53c8f8385dad8b3fa36604f39244a58ac08
shopifyaryan <shopifyaryan82@gmail.com>
2026-03-27 11:07:08 +0530

    Initial commit

---
+++ swiper.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Swiper(".mySwiper", {
+        loop: true,
+
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
+
+
